02767412aa3c72b4bfb94d92004ccba188a2c016
dr-js <dr@dr.run>
2020-04-17 17:26:54 +0800

    dr-graph-editor@0.0.0-dev.2
    notable change:
    - fix code lint for `eslint-config-almost-standard-v14`
    - sort build script
    - package update

--- script/generateSpec.js
+++ script/generateSpec.js
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { writeFileSync } from 'fs'
 
-import { collectSourceRouteMap } from '@dr-js/dev/module/node/export/parse'
+import { collectSourceJsRouteMap } from '@dr-js/dev/module/node/export/parsePreset'
 import { generateExportInfo } from '@dr-js/dev/module/node/export/generate'
 import { /* getMarkdownFileLink, renderMarkdownBlockQuote, */ renderMarkdownAutoAppendHeaderLink, renderMarkdownExportPath } from '@dr-js/dev/module/node/export/renderMarkdown'
 import { runMain } from '@dr-js/dev/module/main'
@@ -12,11 +12,11 @@ const PATH_ROOT = resolve(__dirname, '..')
 const fromRoot = (...args) => resolve(PATH_ROOT, ...args)
 
 runMain(async (logger) => {
-  logger.padLog(`generate exportInfoMap`)
-  const sourceRouteMap = await collectSourceRouteMap({ pathRootList: [ fromRoot('source') ], logger })
+  logger.padLog('generate exportInfoMap')
+  const sourceRouteMap = await collectSourceJsRouteMap({ pathRootList: [ fromRoot('source') ], logger })
   const exportInfoMap = generateExportInfo({ sourceRouteMap })
 
-  logger.log(`output: SPEC.md`)
+  logger.padLog('output: SPEC.md')
   writeFileSync(fromRoot('SPEC.md'), [
     '# Specification',
     '',
--- script/index.js
+++ script/index.js
@@ -1,13 +1,11 @@
 import { resolve } from 'path'
 import { execSync } from 'child_process'
 
-import { getScriptFileListFromPathList } from '@dr-js/dev/module/node/file'
-import { initOutput, packOutput, verifyNoGitignore, verifyGitStatusClean, /* verifyOutputBin, */ publishOutput } from '@dr-js/dev/module/output'
+import { getSourceJsFileListFromPathList } from '@dr-js/dev/module/node/filePreset'
+import { initOutput, packOutput, clearOutput, verifyNoGitignore, verifyGitStatusClean, publishOutput } from '@dr-js/dev/module/output'
 import { getTerserOption, minifyFileListWithTerser } from '@dr-js/dev/module/minify'
 import { processFileList, fileProcessorBabel } from '@dr-js/dev/module/fileProcessor'
 import { runMain, argvFlag } from '@dr-js/dev/module/main'
-
-import { modifyDelete } from '@dr-js/core/module/node/file/Modify'
 
 const PATH_ROOT = resolve(__dirname, '..')
 const PATH_OUTPUT = resolve(__dirname, '../output-gitignore')
@@ -16,7 +14,7 @@ const fromOutput = (...args) => resolve(PATH_OUTPUT, ...args)
 const execShell = (command) => execSync(command, { cwd: fromRoot(), stdio: argvFlag('quiet') ? [ 'ignore', 'ignore', 'inherit' ] : 'inherit' })
 
 const buildOutput = async ({ logger }) => {
-  logger.padLog('generate spec doc')
+  logger.padLog('generate spec')
   execShell('npm run script-generate-spec')
   logger.padLog('build library-babel')
   execShell('npm run build-library-babel')
@@ -25,19 +23,13 @@ const buildOutput = async ({ logger }) => {
 }
 
 const processOutput = async ({ logger }) => {
-  const fileListModule = await getScriptFileListFromPathList([ 'module' ], fromOutput)
-  const fileListLibrary = await getScriptFileListFromPathList([ 'library' ], fromOutput)
+  const fileListModule = await getSourceJsFileListFromPathList([ 'module' ], fromOutput)
+  const fileListLibrary = await getSourceJsFileListFromPathList([ 'library' ], fromOutput)
   let sizeReduce = 0
   sizeReduce += await minifyFileListWithTerser({ fileList: fileListModule, option: getTerserOption({ isReadable: true }), rootPath: PATH_OUTPUT, logger })
   sizeReduce += await minifyFileListWithTerser({ fileList: fileListLibrary, option: getTerserOption(), rootPath: PATH_OUTPUT, logger })
   sizeReduce += await processFileList({ fileList: fileListModule, processor: fileProcessorBabel, rootPath: PATH_OUTPUT, logger })
-  logger.padLog(`total size reduce: ${sizeReduce}B`)
-}
-
-const clearOutput = async ({ logger }) => {
-  logger.log(`clear test`)
-  const fileList = await getScriptFileListFromPathList([ '.' ], fromOutput, (path) => path.endsWith('.test.js'))
-  for (const filePath of fileList) await modifyDelete(filePath)
+  logger.padLog(`size reduce: ${sizeReduce}B`)
 }
 
 runMain(async (logger) => {
@@ -46,19 +38,15 @@ runMain(async (logger) => {
   if (!argvFlag('pack')) return
   await buildOutput({ logger })
   await processOutput({ logger })
-  if (argvFlag('test', 'publish', 'publish-dev')) {
-    logger.padLog(`lint source`)
-    execShell('npm run lint')
-    await processOutput({ logger }) // once more
-    logger.padLog(`test output`)
-    execShell('npm run test-output-library')
-    execShell('npm run test-output-module')
-    // logger.padLog('test browser')
-    // execShell('npm run test-browser')
-  }
-  await clearOutput({ logger })
-  // await verifyOutputBin({ fromOutput, packageJSON, logger })
-  await verifyGitStatusClean({ fromRoot, logger })
+  const isTest = argvFlag('test', 'publish', 'publish-dev')
+  isTest && logger.padLog('lint source')
+  isTest && execShell('npm run lint')
+  isTest && await processOutput({ logger }) // once more
+  isTest && logger.padLog('test output')
+  isTest && execShell('npm run test-output-library')
+  isTest && execShell('npm run test-output-module')
+  await clearOutput({ fromOutput, logger })
+  isTest && await verifyGitStatusClean({ fromRoot, logger })
   const pathPackagePack = await packOutput({ fromRoot, fromOutput, logger })
   await publishOutput({ flagList: process.argv, packageJSON, pathPackagePack, logger })
 })
